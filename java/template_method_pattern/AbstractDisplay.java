d95882208b46c710cf4614430784cddcd04956cd
Takashi Takebayashi <changesworlds@gmail.com>
2016-06-15 04:57:27 +0900

    Add Template Method Pattern using Java

---
+++ java/template_method_pattern/AbstractDisplay.java
@@ -0,0 +1,12 @@
+import java.util.stream.IntStream;
+
+public abstract class AbstractDisplay {
+  public abstract void open();
+  public abstract void print();
+  public abstract void close();
+  public final void display() {
+    open();
+    IntStream.range(0, 5).forEach(i -> print());
+    close();
+  }
+}
