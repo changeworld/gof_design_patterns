64172d62244052029e141277b69e353c02b2119a
Takashi Takebayashi <changesworlds@gmail.com>
2016-06-17 06:18:53 +0900

    Modify ArrayList -> HashMap

--- java/factory_method_pattern/IDCard.java
+++ java/factory_method_pattern/IDCard.java
@@ -1,13 +1,18 @@
 public class IDCard extends Product {
   private String owner;
-  IDCard(String owner) {
-    System.out.println(owner + "のカードを作ります。");
+  private int serial;
+  IDCard(String owner, int serial) {
+    System.out.println(owner + " serial number[" + serial + "]のカードを作ります。");
     this.owner = owner;
+    this.serial = serial;
   }
   public void use() {
-    System.out.println(owner + "のカードを使います。");
+    System.out.println(owner + " serial number[" + serial + "]のカードを使います。");
   }
   public String getOwner() {
     return owner;
+  }
+  public int getSerial() {
+    return serial;
   }
 }
--- java/factory_method_pattern/IDCardFactory.java
+++ java/factory_method_pattern/IDCardFactory.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class IDCardFactory extends Factory {
-  private ArrayList owners = new ArrayList();
+  private HashMap hash = new HashMap();
+  private int serial = 1;
   protected Product createProduct(String owner) {
-    return new IDCard(owner);
+    return new IDCard(owner, serial++);
   }
   protected void registerProduct(Product product) {
-    owners.add(((IDCard)product).getOwner());
+    IDCard card = (IDCard)product;
+    hash.put(card.getSerial(), card.getOwner());
+  }
+  public HashMap getHash() {
+    return hash;
   }
 }
