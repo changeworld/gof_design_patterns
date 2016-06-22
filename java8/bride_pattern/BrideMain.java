b8a6de64bbd72a8f66fbfe1eaa388657e12b32ee
Takashi Takebayashi <changesworlds@gmail.com>
2016-06-23 05:58:06 +0900

    Add RandomCountDisplay to Bride Pattern

--- java8/bride_pattern/BrideMain.java
+++ java8/bride_pattern/BrideMain.java
@@ -3,9 +3,12 @@ public class BrideMain {
     Display d1 = new Display(new StringDisplayImpl("Hello, Japan."));
     Display d2 = new CountDisplay(new StringDisplayImpl("Hello, World."));
     CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+    RandomCountDisplay d4 = new RandomCountDisplay(new StringDisplayImpl("Hello, Galaxy."));
+    int times = 5;
     d1.display();
     d2.display();
     d3.display();
-    d3.multiDisplay(5);
+    d3.multiDisplay(times);
+    d4.randomDisplay(times);
   }
 }
