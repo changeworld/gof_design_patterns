be0fd938dfe1ab3417572ea1e5e630b0846ff048
Takashi Takebayashi <changesworlds@gmail.com>
2016-06-24 06:47:29 +0900

    Add Strategy Pattern using Java

---
+++ java8/strategy_pattern/FoolishStrategy.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class FoolishStrategy implements Strategy {
+  private Random random;
+  private Boolean won = false;
+  private Hand lastHand;
+  public FoolishStrategy(int seed) {
+    random = new Random(seed);
+  }
+  public Hand nextHand() {
+    if (!won) {
+      lastHand = Hand.getHand(random.nextInt(3));
+    }
+    return lastHand;
+  }
+  public void study(boolean win) {
+    won = win;
+  }
+}
